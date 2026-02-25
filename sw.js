d4f6d86528327949685a7434c6b70c43c6a7a881
xavirifa-lgtm <xavi.rifa@googlemail.com>
2026-02-25 12:39:43 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-﻿const CACHE_NAME = 'chronobeats-v1';
+﻿const CACHE_NAME = 'chronobeats-v2';
 const ASSETS = [
   './',
   './index.html',
@@ -9,13 +9,30 @@ const ASSETS = [
   './player_digital_peliculas.html',
   './host_digital_peliculas.html',
   './lista_final_anime.txt',
-  './lista_final_pelis.txt'
+  './lista_final_pelis.txt',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
 ];
 
 self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
       return cache.addAll(ASSETS);
+    })
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
     })
   );
 });
