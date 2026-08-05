37c6b276be46c8bd18a13f21ad09fc6843db5cce
xavirifa-lgtm <xavi.rifa@googlemail.com>
2026-08-05 13:09:47 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-// 🔥🎧 ChronoBeats Online — Service Worker v51 🎧🔥
-const CACHE_NAME = 'chronobeats-v51';
+// 🔥🎧 ChronoBeats Online — Service Worker v52 🎧🔥
+const CACHE_NAME = 'chronobeats-v52';
 
 // Recursos propios a pre-cachear en la instalación (pequeños, sin riesgo)
 const PRECACHE_URLS = [
